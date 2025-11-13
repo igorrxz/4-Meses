ee78889f54eea3f715f3c5f0e52aa0e91e6c4d7f
igorrxz <igorrx24072004@gmail.com>
2025-11-12 21:57:13 -0300

    arrumando caminho

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/4-Meses/', // 👈 ESSA LINHA É A IMPORTANTE
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
